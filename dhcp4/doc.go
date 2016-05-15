e46cf1fec13c248b6ee3369f324258d7c02098d3
David Anderson <dave@natulte.net>
2016-05-15 13:41:39 -0700

    dhcp4: correct import constraint so that we can actually import.

--- dhcp4/doc.go
+++ dhcp4/doc.go
@@ -13,4 +13,4 @@
 // limitations under the License.
 
 // Package dhcp provides building blocks for DHCP clients and servers.
-package dhcp // import "go.universe.tf/netboot/dhcp"
+package dhcp // import "go.universe.tf/netboot/dhcp4"
