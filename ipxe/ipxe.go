28ee099972d059aa70e2cff8afcd288038e3fa8c
Stefan Majer <stefan.majer@f-i-ts.de>
2022-09-10 13:52:14 +0200

    Fix linter

--- ipxe/ipxe.go
+++ ipxe/ipxe.go
@@ -6,11 +6,14 @@ import (
 	"path"
 )
 
+// Files to embed get created during make ipxe
+// nolint:typecheck
+//
 //go:embed ipxe/bin
 var payload embed.FS
 
 func MustGet(name string) []byte {
-	contents, err := payload.ReadFile(path.Join("bin", name))
+	contents, err := payload.ReadFile(path.Join("ipxe/bin", name))
 	if err != nil {
 		panic(err)
 	}
