0d635a115b3c21040f7e81dd0f2ffee157a74e01
Simon Mayer <Simon.Mayer@x-cellent.com>
2025-12-05 18:14:21 +0100

    Test dell with whitespace

--- pixiecore/http.go
+++ pixiecore/http.go
@@ -197,7 +197,8 @@ func ipxeScript(mach Machine, spec *Spec, serverHost string) ([]byte, error) {
 	var b bytes.Buffer
 	b.WriteString("#!ipxe\n")
 	b.WriteString("set console ttyS1\n")
-	b.WriteString("iseq ${manufacturer} :Dell\\ Inc. && set console ttyS0 ||\n")
+	b.WriteString("set sp:hex 20 && set sp ${sp:string}\n")
+	b.WriteString("iseq ${manufacturer} Dell${sp}Inc. && set console ttyS0 ||\n")
 	u := fmt.Sprintf(urlTemplate, url.QueryEscape(string(spec.Kernel)), "kernel", url.QueryEscape(mach.MAC.String()))
 	fmt.Fprintf(&b, "kernel --name kernel %s\n", u)
 	for i, initrd := range spec.Initrd {
